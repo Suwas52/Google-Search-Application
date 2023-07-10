3fd237c9497f6af80cd3bc9d602874a6bcbdb9a4
Suwas52 <suwasdanuwar18@gmail.com>
2023-07-10 23:58:30 +0545

    update search result

--- src/components/SearchedItemTemplate.jsx
+++ src/components/SearchedItemTemplate.jsx
@@ -1,5 +1,16 @@
-const SearchedItemTemplate = () => {
-    return <div>Searched template</div>;
+const SearchedItemTemplate = ({ data }) => {
+  return (
+    <div className="flex flex-col py-3 max-w-[700px] ">
+      <div
+        className="group cursor-pointer"
+        onClick={() => window.open(data.link)}
+      >
+        <div className="text-sm truncate text-[#202124] ">
+          {data.formattedUrl}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SearchedItemTemplate;
